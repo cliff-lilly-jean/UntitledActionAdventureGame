0a465026c65a97a1133f1ed77a19e1affc261071
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2023-03-17 10:12:48 -0400

    initial dev setup

---
+++ Assets/Code/Classes/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // -------------------
+    // METHODS
+    // -------------------
+    // CREATE A WALK METHOD
+    // CREATE A JUMP METHOD
+    // CREATE A CLIMB METHOD
+    // CREATE A RUN METHOD
+    // CREATE AN EAT METHOD
+    // CREATE A FLY METHOD
+    // -------------------
+    // POSSIBLE SYSTEMS
+    // -------------------
+    // CREATE A NEEDS SYSTEM
+    // CREATE A COMBAT SYSTEM
+}
